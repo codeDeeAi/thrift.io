e65b788c38961bcba6b3da5c7a9379069fb8c166
Adeola Ephraim <dunsin.bada@gmail.com>
2022-06-10 09:43:53 +0100

    changed asset to secure assets in page layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,11 @@
 
     <!-- Styles -->
     {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> development --}}
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
 
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> development --}}
-    <script src="/js/app.js" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
 </head>
 
 <body class="font-sans antialiased">
